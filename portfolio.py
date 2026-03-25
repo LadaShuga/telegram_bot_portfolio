afcf4a44c142db69d17a5905d54fa40eddd1b6ed
LadaShuga <96696622+LadaShuga@users.noreply.github.com>
2026-03-25 20:28:08 +0800

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -5,7 +5,7 @@ PORTFOLIO = {
         "name": "⚡️ Бот-парсер вакансий на hh.ru",
         "description": "Собирает данные с сайта hh.ru о вакансиях. Можно ознакомиться с ним по ссылке: @freelance_hunter_parser_bot",
         "tech": "Python, aiogram, sqlite, scheduler",
-        "screens": "hhparse.png",
+        "screens": "screens/hhparse.png",
         "price": "от 5 000 ₽",
         "duration": "1 неделя"
     },
@@ -13,7 +13,7 @@ PORTFOLIO = {
         "name": "📊 Бот для записи в салон красоты",
         "description": "Бот предназначен для онлайн-записи в салон красоты. Можно ознакомиться с ним по ссылке: @TestBookingbot_bot",
         "tech": "Python, aiogram, sqlite, apscheduler",
-        "screens": "bron.png",
+        "screens": "screens/bron.png",
         "price": "от 10 000 ₽",
         "duration": "1 неделя"
     }
